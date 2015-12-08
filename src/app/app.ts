9da46bed463d6a96f2781b6a2ea13f0962ca989c
David <dwgroomes@gmail.com>
2015-12-07 21:24:11 -0600

    use Http

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,26 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, NgFor} from 'angular2/angular2';
+import {Http, HTTP_PROVIDERS} from 'angular2/http'
 
 @Component({
     selector: 'my-app',
-    template: `<h2>hello there</h2>`
+    viewProviders: [HTTP_PROVIDERS],
+    template: `
+        <h2>hello there. here some jokes</h2>
+        <div *ng-for="#joke of jokes">
+            {{joke.joke}}
+        </div>
+    `,
+    directives: [NgFor]
 })
-export class MyAppComponent {}
+export class MyAppComponent {
+
+    jokes:Array<String>;
+
+    constructor(http:Http) {
+        http.get('http://api.icndb.com/jokes/random/3')
+            .map(res => res.json().value)
+            .subscribe(jokes => this.jokes = jokes)
+    }
+}
 
 bootstrap(MyAppComponent);
