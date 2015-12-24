ff8837d2b172959bed83e718397f19ef1b47fde7
David <dwgroomes@gmail.com>
2015-12-24 14:32:10 -0600

    sloppy implementation of a Webpack workflow

--- app/boot.ts
+++ app/boot.ts
@@ -1,3 +1,6 @@
+// inspired by https://github.com/AngularClass/angular2-webpack-starter
+// Angular 2
+
 import {bootstrap} from 'angular2/platform/browser';
 import {MyAppComponent} from './app.component'
 
---
+++ app/vendor.ts
@@ -0,0 +1,13 @@
+//import 'zone.js';
+import 'zone.js/lib/browser/zone-microtask';
+import 'reflect-metadata';
+
+// Angular 2
+import 'angular2/platform/browser';
+import 'angular2/platform/common_dom';
+import 'angular2/core';
+import 'angular2/router';
+import 'angular2/http';
+
+// RxJS
+import 'rxjs';
