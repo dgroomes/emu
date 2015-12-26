d923af41fac6e86c14b2b790b5d27ab0861e03a7
David <dwgroomes@gmail.com>
2015-12-26 15:47:32 -0600

    Later event for startup listener

--- src/main/kotlin/us/mn/dgtc/emu/Listeners.kt
+++ src/main/kotlin/us/mn/dgtc/emu/Listeners.kt
@@ -2,6 +2,7 @@ package us.mn.dgtc.emu;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,10 @@ import org.springframework.stereotype.Component;
  * Event listeners. For fun.
  */
 @Component
-class Listeners @Autowired constructor(@Value("\${host.url}") val hostUrl: String, @Value("\${startup.endpoints}") val startupEndpoints: Array<String>) {
+class Listeners @Autowired constructor(@Value("\${host.url}") val hostUrl: String,
+                                       @Value("\${startup.endpoints}") val startupEndpoints: Array<String>) {
 
-    @EventListener(ContextRefreshedEvent::class)
+    @EventListener(ApplicationReadyEvent::class)
     fun listUsefulStartupEndpoints() = startupEndpoints.forEach {
         println("check out: ${hostUrl + it}")
     }
